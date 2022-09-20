d7b917747961a0047f5db64a194203f38a0e96f7
Michal_Pallay_skola <pallayovci@gmail.com>
2022-09-20 08:50:02 +0200

    skola

--- zaklady/qad_formula.py
+++ zaklady/qad_formula.py
@@ -1,11 +1,14 @@
 from math import *
-a = -5
+a = 5
 b = 3
 c = 2
-
-def form(a,b,c):
-    x1 = (-b + sqrt(b**2 - 4*a*c)) / 2*a
-    x2 = (-b - sqrt(b**2 - 4*a*c)) / 2*a
+def kvadraticka_rovnica(a, b, c):
+    x1 = (-b + sqrt((b**2) - (4*a*c))) / (2*a)
+    x2 = (-b - sqrt((b**2) - (4*a*c))) / (2*a)
 
     return x1,x2
-print(form(a,b,c))
+
+try:
+    print(kvadraticka_rovnica(a, b, c))
+except:
+    print("deskriminat nema")
