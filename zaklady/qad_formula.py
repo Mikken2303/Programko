d2f1c9e659c1c23703b75d5e5f5247b5c69c1066
Michal_Pallay_skola <pallayovci@gmail.com>
2022-09-20 09:10:24 +0200

    skola

--- zaklady/qad_formula.py
+++ zaklady/qad_formula.py
@@ -1,14 +1,14 @@
 from math import *
-a = 5
-b = 3
+a = 2
+b = 4
 c = 2
 def kvadraticka_rovnica(a, b, c):
     x1 = (-b + sqrt((b**2) - (4*a*c))) / (2*a)
     x2 = (-b - sqrt((b**2) - (4*a*c))) / (2*a)
 
     return x1,x2
-
 try:
     print(kvadraticka_rovnica(a, b, c))
+
 except:
-    print("deskriminat nema")
+    print("discriminat < 0")
---
+++ zaklady/trojuholnik.py
@@ -0,0 +1,12 @@
+a = 3
+b = 4
+c = 5
+
+if (a + b) > c or  (a + c) > b or (b + c) > a:
+    print("existuje")
+    if a == b == c:
+        print("rovnostranny")
+    elif a == b or a == c or b == c:
+        print("rovnoramenny")
+    elif (a**2 + b**2) == c**2 or  (a**2 + c**2) == b**2 or (b**2 + c**2) == a**2:
+        print("pravouhly")
