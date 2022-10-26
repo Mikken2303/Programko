f644c0e62f57630a483355072c61109faaf3b5b0
Michal Pallay <pallayovci@gmail.com>
2022-10-26 20:18:17 +0200

    made ping pong

--- maturita/2n-znamky.py
+++ maturita/2n-znamky.py
@@ -1,19 +1,17 @@
-# sam neviem co som robil xd
-# note to self treba prerobit + prirobit ci prospel neprosl atd
-# ale ide to so shut up
 
 list = []
 ip = True
-buf = float(0)
+pocitadlo = float(0)
 max = 0
 counter = 0
+
 while ip != 0:
     counter += 1
     ip = int(input(': '))
-    buf += ip
+    pocitadlo += ip
     if ip > max:
         max = ip
 
-print(max)
-print(buf/ (counter - 1))
+print(f"najhorsia znamka bola {max}. ")
+print(f"priemer bol {pocitadlo / (counter - 1)}")
 
