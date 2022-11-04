df11eec9999654e36c79f5b4b719d5f1d9e20409
Michal Pallay <pallayovci@gmail.com>
2022-11-04 08:45:39 +0100

    it works probs

---
+++ maturita/3-cezar.py
@@ -0,0 +1,13 @@
+text = "CEASER CIPHER DEMO"
+s = 4
+result = ""
+for i in range(len(text)):
+    char = text[i]
+    if (char.isupper()):
+        result += chr((ord(char) + s - 65) % 26 + 65)
+    else:
+        result += chr((ord(char) + s - 97) % 26 + 97)
+print(result)
+
+
+
