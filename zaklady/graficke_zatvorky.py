6a0e02665b66a6b8bf10955d1f284e835990fe47
Michal_Pallay_skola <pallayovci@gmail.com>
2023-01-17 08:54:10 +0100

    huk

--- zaklady/graficke_zatvorky.py
+++ zaklady/graficke_zatvorky.py
@@ -4,31 +4,28 @@ canvas.pack()
 vyr = "4x((38+(34-5y)3x)+84).(14-5x)(16+3y)(-8-3x)"
 c = 0
 good = True
+x = 25
+y = 25
+k = 0
+farby =  ['red' , 'green' , 'blue' , 'cyan' , 'yellow' ,'magenta',"orange","purple"]
+
 for i in vyr:
     if i == "(":
+        canvas.create_text(x, y, text=i, fill=farby[k])
+        k += 1
         c += 1
     elif i == ")":
+        k += -1
+        canvas.create_text(x, y, text=i, fill=farby[k])
         c += -1
+    else:
+        canvas.create_text(x, y, text=i)
+    x += 8
+    #if c == 0:
+    #    k += 1
     if c < 0:
         print("zle zatvorky")
         good = False
         break
-x = 25
-y = 25
-k = 0
-
-farby =  ['red' , 'green' , 'blue' , 'cyan' , 'yellow' ,'magenta']
-for i in vyr:
-    if i == "(":
-        canvas.create_text(x, y,text=i,fill=farby[k])
-        k += 1
-    elif i == ")":
-        k += -1
-        canvas.create_text(x, y,text=i,fill=farby[k])
-    else:
-        canvas.create_text(x, y,text=i)
-
-    x += 8
-
 canvas.mainloop()
 
