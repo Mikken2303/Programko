5064f944885ce95541c3cf0eadb34db2a2bc8be0
Michal_Pallay_skola <pallayovci@gmail.com>
2023-02-16 13:16:02 +0100

    huk

--- maturita/1-10/1-trojuholnik.py
+++ maturita/1-10/1-trojuholnik.py
@@ -3,13 +3,13 @@ a = int(input("1. strana trojuholnika: "))
 b = int(input("2. strana trojuholnika: "))
 c = int(input("3. strana trojuholnika: "))
 
-#not sure ci tam nema byt and namiesto or
+
 
 if (a + b) > c and (a + c) > b and (b + c) > a:
     print("existuje")
     if a == b == c:
         print("rovnostranny")
-    elif a == b or a == c or b == c:
+    elif a == b and a == c and b == c:
         print("rovnoramenny")
     if (a**2 + b**2) == c**2 or  (a**2 + c**2) == b**2 or (b**2 + c**2) == a**2:
         print("pravouhly")
