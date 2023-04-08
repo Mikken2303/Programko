d3faad8151c7098784efa559a51666f033044005
Michal Pallay <pallayovci@gmail.com>
2023-04-08 14:01:59 +0200

    it works probs

---
+++ fun/factorization.py
@@ -0,0 +1,21 @@
+inp = 3
+fact = []
+primes = []
+c = 2
+b = True
+while inp != 1 or b:
+    p = True
+    for i in primes:
+        if c % i == 0:
+            p = False
+            break
+    if p:
+        primes.append(c)
+        while inp % c == 0:
+            fact.append(c)
+            inp = inp / c
+    if c > inp:
+        fact.append(inp)
+        b = False
+    c += 1
+print(fact)
